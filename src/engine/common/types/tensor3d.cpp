7b886a555da8cf23bb9fda82105f9c1082a4763a
abhishek-jatram <jatramabh@gmail.com>
2023-01-22 19:16:35 +0530

    Optimized Transpose method of Tensor3D
    Previously on every transpose_value call, all the if checks were done in the worst case.
    With the new changes, we completely avoid if checks saving a huge runtime.

--- src/engine/common/types/tensor3d.cpp
+++ src/engine/common/types/tensor3d.cpp
@@ -72,26 +72,36 @@ template <typename T>
 Tensor3D<T> Tensor3D<T>::Transpose(Dimension dim1, Dimension dim2, Dimension dim3) {
     assert(dim1 != dim2 && dim1 != dim3 && dim2 != dim3 &&
         "Expected distinct dimentions for transpose");
+    // XYZ -> XYZ // Default
     auto transposed_value = [&](int i, int j, int k) -> T& {
-        // XYZ -> XYZ
-        if (dim1 == Dimension::X && dim2 == Dimension::Y && dim3 == Dimension::Z)
-            return (*this)(i,j,k);
-        // XYZ -> XZY
-        if (dim1 == Dimension::X && dim2 == Dimension::Z && dim3 == Dimension::Y)
-            return (*this)(i,k,j);
-        // XYZ -> YXZ
-        if (dim1 == Dimension::Y && dim2 == Dimension::X && dim3 == Dimension::Z)
-            return (*this)(j,i,k);
-        // XYZ -> YZX
-        if (dim1 == Dimension::Y && dim2 == Dimension::Z && dim3 == Dimension::X)
-            return (*this)(j,k,i);
-        // XYZ -> ZXY
-        if (dim1 == Dimension::Z && dim2 == Dimension::X && dim3 == Dimension::Y)
-            return (*this)(k,i,j);
-        // XYZ -> ZYX
-        if (dim1 == Dimension::Z && dim2 == Dimension::Y && dim3 == Dimension::X)
-            return (*this)(k,j,i);
+        return (*this)(i,j,k);
     };
+
+    // XYZ -> XZY
+    if (dim1 == Dimension::X && dim2 == Dimension::Z && dim3 == Dimension::Y)
+        transposed_value = [&](int i, int j, int k) -> T& {
+            return (*this)(i,k,j);
+        };
+    // XYZ -> YXZ
+    if (dim1 == Dimension::Y && dim2 == Dimension::X && dim3 == Dimension::Z)
+        transposed_value = [&](int i, int j, int k) -> T& {
+            return (*this)(j,i,k);
+        };
+    // XYZ -> YZX
+    if (dim1 == Dimension::Y && dim2 == Dimension::Z && dim3 == Dimension::X)
+        transposed_value = [&](int i, int j, int k) -> T& {
+            return (*this)(j,k,i);
+        };
+    // XYZ -> ZXY
+    if (dim1 == Dimension::Z && dim2 == Dimension::X && dim3 == Dimension::Y)
+        transposed_value = [&](int i, int j, int k) -> T& {
+            return (*this)(k,i,j);
+        };
+    // XYZ -> ZYX
+    if (dim1 == Dimension::Z && dim2 == Dimension::Y && dim3 == Dimension::X)
+        transposed_value = [&](int i, int j, int k) -> T& {
+            return (*this)(k,j,i);
+        };
 
     Tensor3D tensor(GetLength(dim1), GetLength(dim2), GetLength(dim3));
     for (int i=0; i<tensor.GetLength(Dimension::X); i++) {
